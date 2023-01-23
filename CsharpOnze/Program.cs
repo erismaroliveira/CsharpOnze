16c6be57d0ebbb13c4a4ce352b5f020bda22737c
Erismar Oliveira <erismarpro@hotmail.com>
2023-01-23 09:04:52 -0300

    Initial commit

---
+++ CsharpOnze/Program.cs
@@ -0,0 +1,23 @@
+﻿var m1 = new Measurement();
+Console.WriteLine(m1);
+
+var m2 = default(Measurement);
+Console.WriteLine(m2);
+
+var ms = new Measurement[2];
+Console.WriteLine(string.Join(", ", ms));
+
+public struct Measurement
+{
+    public Measurement()
+    {
+        Value = Double.NaN;
+        Description = "Undefined";
+    }
+    
+    public double Value { get; init; }
+    public string Description { get; init; }
+
+    public override string ToString()
+        => $"{Value} ({Description})";
+}
